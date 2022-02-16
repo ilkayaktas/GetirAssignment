8a7d03451710ea6dde86e1f1f2688b597d60511b
İlkay AKTAŞ <iaktas@aselsan.com.tr>
2022-02-16 11:33:06 +0300

    Spring version is decreased because of swagger compatibility. JWT authorization is added but not integrated to security constraints. Swagger ui is integrated.

---
+++ src/main/java/com/ilkayaktas/readingisgoodgetir/model/SpringFoxConfig.java
@@ -0,0 +1,14 @@
+package com.ilkayaktas.readingisgoodgetir.model;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by ilkayaktas on 13.02.2022 at 10:53.
+ */
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+
+}
--- src/main/java/com/ilkayaktas/readingisgoodgetir/security/SpringSecurityConfig.java
+++ src/main/java/com/ilkayaktas/readingisgoodgetir/security/SpringSecurityConfig.java
@@ -2,8 +2,10 @@ package com.ilkayaktas.readingisgoodgetir.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -27,6 +29,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
               .httpBasic()
               .and()
               .authorizeRequests()
+              .antMatchers(HttpMethod.GET,"/swagger-ui").permitAll()
               .anyRequest()
               .authenticated()
               .and()
@@ -34,5 +37,17 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
               .formLogin().disable();
    }
 
+   @Override
+   public void configure(WebSecurity web) throws Exception {
+      web.ignoring().antMatchers("/v3/api-docs",
+              "/configuration/ui",
+              "/swagger-ui/",
+              "/swagger-ui/**",
+              "/swagger-resources/**",
+              "/configuration/security",
+              "/swagger-ui.html",
+              "/webjars/**");
+
+   }
 
 }
